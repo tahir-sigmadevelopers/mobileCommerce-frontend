bbb35c2c39c5dc150c53d4f781fd97b9398d3ebd
Mohammad Tahir <starghareeb@gmail.com>
2025-03-05 23:09:24 +0500

    admin dashboard redirection done ...

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,5 +1,4 @@
 import { BiMaleFemale } from "react-icons/bi";
-import { BsSearch } from "react-icons/bs";
 import { FaRegBell } from "react-icons/fa";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 import { useSelector } from "react-redux";
